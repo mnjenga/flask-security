8d9650bdaf2a099afa87e356363248e79c945390
Moses Njenga <moses@abigail.co.ke>
2018-01-04 21:06:40 +0300

    Update forms.py

--- flask_security/forms.py
+++ flask_security/forms.py
@@ -99,7 +99,7 @@ class APIForm(BaseForm):
     def __init__(self, *args, **kwargs):
         if current_app.testing:
             self.TIME_LIMIT = None
-        super(Form, self).__init__(*args, **kwargs)
+        super(APIForm, self).__init__(*args, **kwargs)
 
 
 
