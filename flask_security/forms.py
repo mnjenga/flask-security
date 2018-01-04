3f362c85ee5be8ed5e5600c2bfe002ef61a188e5
Moses Njenga <moses@abigail.co.ke>
2018-01-04 21:26:51 +0300

    Update forms.py

--- flask_security/forms.py
+++ flask_security/forms.py
@@ -212,7 +212,58 @@ class PasswordlessLoginForm(Form, UserEmailFormMixin):
         return True
 
 
-class LoginForm(APIForm, NextFormMixin):
+class LoginFormForm(Form, NextFormMixin):
+    """The default login form"""
+
+    email = StringField(get_form_field_label('email'),
+                        validators=[Required(message='EMAIL_NOT_PROVIDED')])
+    password = PasswordField(get_form_field_label('password'),
+                             validators=[password_required])
+    remember = BooleanField(get_form_field_label('remember_me'))
+    submit = SubmitField(get_form_field_label('login'))
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        if not self.next.data:
+            self.next.data = request.args.get('next', '')
+        self.remember.default = config_value('DEFAULT_REMEMBER_ME')
+        if current_app.extensions['security'].recoverable and \
+                not self.password.description:
+            html = Markup(u'<a href="{url}">{message}</a>'.format(
+                url=url_for_security("forgot_password"),
+                message=get_message("FORGOT_PASSWORD")[0],
+            ))
+            self.password.description = html
+
+    def validate(self):
+        if not super(LoginForm, self).validate():
+            return False
+
+        self.user = _datastore.get_user(self.email.data)
+
+        if self.user is None:
+            self.email.errors.append(get_message('USER_DOES_NOT_EXIST')[0])
+            # Reduce timing variation between existing and non-existung users
+            hash_password(self.password.data)
+            return False
+        if not self.user.password:
+            self.password.errors.append(get_message('PASSWORD_NOT_SET')[0])
+            # Reduce timing variation between existing and non-existung users
+            hash_password(self.password.data)
+            return False
+        if not verify_and_update_password(self.password.data, self.user):
+            self.password.errors.append(get_message('INVALID_PASSWORD')[0])
+            return False
+        if requires_confirmation(self.user):
+            self.email.errors.append(get_message('CONFIRMATION_REQUIRED')[0])
+            return False
+        if not self.user.is_active:
+            self.email.errors.append(get_message('DISABLED_ACCOUNT')[0])
+            return False
+        return True
+
+ 
+class ApiLoginForm(APIForm, NextFormMixin):
     """The default login form"""
 
     email = StringField(get_form_field_label('email'),
